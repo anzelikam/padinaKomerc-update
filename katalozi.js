f4b30d9655b05e62c7da82afb23c8d40cdbaf997
anzelikam <anzelikamarko@gmail.com>
2024-04-24 20:14:56 +0200

    convert to json

--- katalozi.js
+++ katalozi.js
@@ -1,7 +1,7 @@
-const katalozi: [
+
       {
-        src: "https://github.com/anzelikam/padinaKomerc-update/blob/main/matis-katalog.jpg?raw=true",
-        pdfLink:
+        "src": "https://github.com/anzelikam/padinaKomerc-update/blob/main/matis-katalog.jpg?raw=true",
+        "pdfLink":
           "https://matis.rs/wp-content/uploads/2024/03/Flajer-56-strana.pdf",
       },
-    ],
+    
