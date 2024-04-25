07863e956b96b87adb06ba6390461e798b3c49c9
anzelikam <anzelikamarko@gmail.com>
2024-04-25 21:37:18 +0200

    Update katalozi.js

--- katalozi.js
+++ katalozi.js
@@ -9,8 +9,8 @@
       { "src": "https://github.com/anzelikam/padinaKomerc-update/blob/main/djuka-katalog.jpg?raw=true",
        "pdfLink": "https://djukapromet.com/wp-content/uploads/2020/05/Katalog-2020.pdf"
       },
-      { "src": "https://github.com/anzelikam/padinaKomerc-update/blob/main/jela-katalog.jpg?raw=true",
-       "pdfLink": "https://jela.rs/pub/download/JELA-Katalog-2020-rs.pdf"
+      { "src": "https://github.com/anzelikam/padinaKomerc-update/blob/main/beli-bor-katalog.jpg?raw=true",
+       "pdfLink": "https://beli-bor.rs/"
       }
 ]
     
