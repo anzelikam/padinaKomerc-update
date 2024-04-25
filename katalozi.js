eadcb5bb916eeb8cb7d3c95a6e002aaf9f2ed3fc
anzelikam <anzelikamarko@gmail.com>
2024-04-25 05:42:34 +0200

    Update katalozi.js

--- katalozi.js
+++ katalozi.js
@@ -1,7 +1,8 @@
-
+"
       {
         "src": "https://github.com/anzelikam/padinaKomerc-update/blob/main/matis-katalog.jpg?raw=true",
         "pdfLink": "https://matis.rs/wp-content/uploads/2024/03/Flajer-56-strana.pdf",
       }
+"
     
 
