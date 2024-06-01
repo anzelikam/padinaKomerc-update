e4fb40c21144b7cc4dd04aa353e7139b31e30607
anzelikam <anzelikamarko@gmail.com>
2024-06-01 23:04:49 +0200

    Update katalozi.js jun

--- katalozi.js
+++ katalozi.js
@@ -1,7 +1,7 @@
 [
       {
         "src": "https://github.com/anzelikam/padinaKomerc-update/blob/main/matis-katalog.jpg?raw=true",
-        "pdfLink": "https://matis.rs/wp-content/uploads/2024/04/Flajer-56-strana-1.pdf"
+        "pdfLink": "https://matis.rs/wp-content/uploads/2024/05/Flajer-jun.pdf"
       },
       { "src": "https://github.com/anzelikam/padinaKomerc-update/blob/main/jela-katalog.jpg?raw=true",
        "pdfLink": "https://jela.rs/pub/download/JELA-Katalog-2020-rs.pdf"
