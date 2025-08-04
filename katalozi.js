d16db2d06dec08412f2b108c22ad64271e75678b
anzelikam <anzelikamarko@gmail.com>
2025-08-04 21:20:54 +0200

    Update katalozi.js
    august-update

--- katalozi.js
+++ katalozi.js
@@ -1,7 +1,7 @@
 [
       {
         "src": "https://github.com/anzelikam/padinaKomerc-update/blob/main/matis-katalog.jpg?raw=true",
-        "pdfLink":"https://matis.rs/wp-content/uploads/2025/05/Akcija-jun.pdf"
+        "pdfLink":"https://matis.rs/wp-content/uploads/2025/08/Veliki-flajer.pdf"
       },
       { "src": "https://github.com/anzelikam/padinaKomerc-update/blob/main/jela-katalog.jpg?raw=true",
        "pdfLink": "https://jela.rs/pub/download/JELA-Katalog-2020-rs.pdf"
